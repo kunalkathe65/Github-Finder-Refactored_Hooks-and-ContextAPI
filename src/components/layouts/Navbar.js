1f38e10f29779c694148dcfc256bf950a64e4e15
kunalkathe65 <kunalkathe65@gmail.com>
2019-09-22 18:24:58 +0530

    person is again a class comp & search btn added

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import 'font-awesome/css/font-awesome.min.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "font-awesome/css/font-awesome.min.css";
 
-const Navbar = (props) => {
-    Navbar.defaultProps = {             //default props
-        title : 'Github Finder',
-        icon : 'fa fa-github'
-      };
+class Navbar extends Component {
+  defaultProps = {
+    //default props
+    title: "Github Finder",
+    icon: "fa fa-github"
+  };
 
-    Navbar.propTypes = {                  //typechecking props
-        title : PropTypes.string.isRequired,
-        icon : PropTypes.string.isRequired
-    };
-
-    return(
-            <div className="navbar bg-primary">
-                <h1>
-                    <i className={props.icon}></i> {props.title}
-                </h1>
-            </div>
-        );
-    }
+  propTypes = {
+    //typechecking props
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+  render() {
+    return (
+      <div className="navbar bg-primary">
+        <h1>
+          <i className={this.defaultProps.icon}></i> {this.defaultProps.title}
+        </h1>
+      </div>
+    );
+  }
+}
 export default Navbar;
