19e7ca3d8b78ecb198c0d54106929465c1bacb5a
kunalkathe65 <kunalkathe65@gmail.com>
2019-09-22 09:12:11 +0530

    font-awesome package  added

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,10 +1,11 @@
 import React ,{Component} from 'react';
 import PropTypes from 'prop-types';
+import 'font-awesome/css/font-awesome.min.css';
 
 class Navbar extends Component{
     static defaultProps = {             //default props
         title : 'Github Finder',
-        icon : 'fa-github'
+        icon : 'fa fa-github'
       };
 
     static propTypes = {                  //typechecking props
@@ -16,7 +17,7 @@ class Navbar extends Component{
         return(
             <div className="navbar bg-primary">
                 <h1>
-                    <i className={this.props.icon} /> {this.props.title}
+                    <i className={this.props.icon}></i> {this.props.title}
                 </h1>
             </div>
         );
