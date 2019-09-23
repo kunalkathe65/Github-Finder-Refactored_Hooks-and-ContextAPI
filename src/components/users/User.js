7b96ddfc60e5aa9626559a2c01b5ad5e5dd52f6d
kunalkathe65 <kunalkathe65@gmail.com>
2019-09-23 10:44:13 +0530

    added user details in card and  Back button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import Spinner from "../layouts/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
   render() {
     const {
       name,
@@ -19,9 +29,58 @@ export class User extends Component {
       hireable
     } = this.props.user;
 
-    //const { loading } = this.props.loading;
+    if (this.props.loading) return <Spinner />;
 
-    return <div>{name}</div>;
+    return (
+      <Fragment>
+        Hireable : {""}
+        {hireable ? (
+          <i className="fa fa-check text-success" />
+        ) : (
+          <i className="fa fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              alt=""
+              className="round-img"
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>{location}</p>
+          </div>
+          <div>
+            {login && (
+              <Fragment>
+                <p>
+                  <b>Username :</b> {login}
+                </p>
+              </Fragment>
+            )}
+            {bio && (
+              <Fragment>
+                <p>
+                  <b>Bio :</b> {bio}
+                </p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers : {followers}</div>
+          <div className="badge badge-success">Following : {following}</div>
+          <div className="badge badge-light">Public Repos : {public_repos}</div>
+          <div className="badge badge-dark">Public Gists : {public_gists}</div>
+        </div>
+        <Link to="/" className="btn btn-dark">
+          Back
+        </Link>
+      </Fragment>
+    );
   }
 }
 
